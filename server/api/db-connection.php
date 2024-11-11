cdf027015b2ba53e967ab214b5a30e8dd3d26813
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-11 23:11:06 +0200

    php <> connection added

--- server/api/db-connection.php
+++ server/api/db-connection.php
@@ -4,6 +4,20 @@ header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
 
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "AI-Movie-Recommender";
 
+try {
+    $connection = new mysqli($host, $username, $password, $dbname);
 
+    if ($connection->connect_errno) {
+        throw new Exception("Failed to connect to the database: " . $connection->connect_error);
+    }
+    echo "Connected successfully to the database.";
+} catch (Exception $e) {
+
+    die("Database connection error: " . $e->getMessage());
+}
 ?>
