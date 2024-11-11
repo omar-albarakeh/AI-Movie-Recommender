76b6132d3967b2239502f18ddbdfcac635c488aa
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-11 23:46:57 +0200

    login <> php querry updated(1)

--- server/api/login.php
+++ server/api/login.php
@@ -9,6 +9,24 @@ $query->bind_param("s", $username);
 $query->execute();
 
 $result = $query->get_result();
+if($result->num_rows != 0) {
+  $user = $result->fetch_assoc();
 
+  $check = password_verify($password, $user["password"]);
 
-}?>
+  if($check){
+    echo json_encode([
+      "status" => "Login Successful",
+      "user" => $user,
+    ]);
+  } else {
+    echo json_encode([
+      "status" => "Invalid Credentials",
+    ]);
+  }
+} else {
+  echo json_encode([
+    "status" => "Invalid Credentials",
+  ]);
+}}
+?>
