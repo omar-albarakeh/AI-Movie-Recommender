41eed96bd8bf58d91c875c89ad12d3969d1c641e
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-11 23:09:29 +0200

    php <> headers added

---
+++ server/api/db-connection.php
@@ -0,0 +1,9 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json");
+
+
+
+?>
