0cd976f42b70da9acd06f4b7d50a859df3d35976
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-12 08:57:05 +0200

    js <> variables added

---
+++ front-end/chatbot/main.js
@@ -0,0 +1,5 @@
+
+const chatContainer = document.getElementById("chat-container");
+const userInput = document.getElementById("input-text");
+const sendBtn = document.getElementById("send-btn");
+const historyPanel = document.getElementById("saved-responses");
