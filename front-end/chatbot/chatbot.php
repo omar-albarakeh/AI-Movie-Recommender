f362461a54a7b41598d743329ecddb344161b94e
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-12 08:30:06 +0200

    chatbot <> api key added

--- front-end/chatbot/chatbot.php
+++ front-end/chatbot/chatbot.php
@@ -7,4 +7,13 @@ header("Content-Type: application/json");
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
     exit;
-}?>
+}
+$apiKey = "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";
+
+
+
+
+
+
+
+?>
