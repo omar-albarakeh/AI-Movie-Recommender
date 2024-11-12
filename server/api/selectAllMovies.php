a1d50842e922460f0b560cbe15cfe6b7f14ac9e8
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-12 21:47:07 +0200

    backend <> creating select all movies api

---
+++ server/api/selectAllMovies.php
@@ -0,0 +1,21 @@
+<?php
+
+include("connection.php");
+
+
+
+$query = $connection->prepare("SELECT id,title FROM movies");
+$query->execute();
+
+$result = $query->get_result();
+
+if ($result->num_rows > 0) {
+    $movies_array = [];
+    while ($resultObject = $result->fetch_assoc()) {
+        $movies_array[] = $resultObject;
+    }
+
+    echo json_encode($movies_array);
+} else {
+    echo json_encode([]);
+}
