0d089af446ee167d78677bb28e1073bd6c998a43
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-12 07:56:35 +0200

    login <> php logic added

---
+++ server/api/login.php
@@ -0,0 +1,26 @@
+<?php
+include "../db_connection.php";
+
+    $input = json_decode(file_get_contents("php://input"), true);
+    $username = $input['username'] ?? '';
+    $password = $input['password'];
+
+    $stmt = $connection->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows === 1) {
+        $user = $result->fetch_assoc();
+        
+        if (password_verify($password, $user['password'])) { 
+            echo json_encode(["success" => true, "message" => "Login successful."]);
+        } else {
+            echo json_encode(["success" => false, "message" => "Invalid password."]);
+        }
+    } else {
+        echo json_encode(["success" => false, "message" => "User not found."]);
+    }
+
+ 
+?>
