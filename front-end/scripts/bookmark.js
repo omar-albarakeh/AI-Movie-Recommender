113f1aa61645ccfbf942b9ffa0815a33f4aeb005
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-13 01:46:27 +0200

    frontend <> editing logic to add bookmark to database

--- front-end/scripts/bookmark.js
+++ front-end/scripts/bookmark.js
@@ -3,8 +3,14 @@ const addBookmark = async (movies_id,users_id=1) => {
     const data = new FormData()
     data.append("users_id", users_id)
     data.append("movies_id", movies_id)
-    await axios.post(
-        "http://127.0.0.1:8080/AI-Movie-Recommender/server/api/insertBookmarks.php",
+
+    const response = await axios.post(
+        "http://localhost:8080/AI-Movie-Recommender/server/api/isExistingBookmarks.php",
+        data
+    ); 
+       
+    !response.data && await axios.post(
+        "http://localhost:8080/AI-Movie-Recommender/server/api/insertBookmarks.php",
         data
     ); 
     
