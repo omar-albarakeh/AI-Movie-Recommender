1b99acb44755b86f236ee769781509d700c93005
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-13 10:35:37 +0200

    frontend <> creating getBookmarked function to track bookmarked from database on load

--- front-end/scripts/bookmark.js
+++ front-end/scripts/bookmark.js
@@ -26,5 +26,25 @@ const toggleBookmark = async (movies_id,users_id=1) => {
     
   };
 
+const getBookmarked = async (users_id=1) =>{
+    
+    const data = new FormData()
+    data.append("users_id", users_id)
+    
+    const response = await axios.post(
+    "http://localhost:8080/AI-Movie-Recommender/server/api/selectBookmarks.php",
+    data
+    );    
+    let bookmarks = response.data
+    bookmarks.forEach(bookmark=>{
+        const card = document.getElementById(bookmark.movies_id)
+        card.querySelector(".bookmark-icon").src = "./assets/filledBookmark.png"
+
+    })
+
+}
+// bookmark on load
+getBookmarked()
+
 
 
