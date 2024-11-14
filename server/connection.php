60de9ed048b297b9f277af52f4147c670fcdc681
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-14 02:27:49 +0200

    path updated

---
+++ server/connection.php
@@ -0,0 +1,16 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET,POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
+
+$host = "localhost";
+$dbuser = "root";
+$pass = "";
+$dbname = "ai_enhanced_movie_recommenderdb";
+
+$connection = new mysqli($host,$dbuser,$pass,$dbname);
+
+if ($connection->connect_error)
+    die("Error occured");
