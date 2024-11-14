548cfbe0c283b45e3a516bbb38d066efcc4dd729
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-14 06:03:04 +0200

    backend <> editing select recommendations to get distinct values

--- front-end/scripts/stars.js
+++ front-end/scripts/stars.js
@@ -11,19 +11,21 @@ const postStars = async (movies_id,rating,users_id = 1) =>{
     )
 }
 
-const getBookmarked = async (users_id=1) =>{
+const getStars = async (users_id=1) =>{
     
     const data = new FormData()
     data.append("users_id", users_id)
     
     const response = await axios.post(
-    "http://localhost:8080/AI-Movie-Recommender/server/api/selectBookmarks.php",
+    "http://localhost:8080/AI-Movie-Recommender/server/api/selectRatingsByUserId.php",
     data
     );    
-    let bookmarks = response.data
-    bookmarks.forEach(bookmark=>{
-        const card = document.querySelector(`[movieId="${bookmark.movies_id}"]`);
-        card.querySelector(".bookmark-icon").src = "./assets/filledBookmark.png"
+    let ratings = response.data
+    await ratings.forEach(rating=>{
+        const card = document.querySelector(`[movieId="${rating.movies_id}"]`);
+        const stars = card.querySelectorAll(".star")
+        for(let i=0;i<rating.rating;i++)
+            stars[i].src = "./assets/filledStar.png"
 
     })
 
